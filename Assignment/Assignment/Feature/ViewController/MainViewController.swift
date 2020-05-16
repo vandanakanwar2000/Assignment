366866a92b6c7c1ac355e92cd11fcde2341fa812
Vandana Kanwar <vandana.kanwar@westpac.com.au>
2020-05-16 20:13:24 +1000

    Added New Xcode assignment Project and removed main stroryboard

---
+++ Assignment/Assignment/Feature/ViewController/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Assignment
+//
+//  Created by Vandana Kanwar on 16/5/20.
+//  Copyright © 2020 Vandana Kanwar. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+}
